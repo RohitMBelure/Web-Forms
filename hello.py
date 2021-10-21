551d2fc3ee05cc7f1300a5977fc9f69ed7335c38
Rohit <rohitbelure128@gmail.com>
2021-10-21 19:33:25 +0530

    Redirects and user sessions

--- hello.py
+++ hello.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name=None
-	form=NameForm()
+	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', form=form, name=name)
-	
+		session['name'] = form.name.data
+	return render_template('index.html', form=form, name=session.get('name'))
+
 if __name__ == '__main__':
 	app.run(debug=True)
