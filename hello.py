6c4a796c79f0acc86066c1771866333102a4b734
Rohit <rohitbelure128@gmail.com>
2021-10-21 19:17:48 +0530

    Route methods

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+app = Flask(__name__)
 
-app =  Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-
-class NameForm(Form):
-	name = stringField('What is your name?', validators=[Reuired()])
-	submit = SubmitField('Submit')
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name=None
+	form=NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
+	
 if __name__ == '__main__':
 	app.run(debug=True)
