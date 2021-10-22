0946402fffed4033c1e5404ca30c67f7b0d5bd7c
Rohit <rohitbelure128@gmail.com>
2021-10-22 13:01:58 +0530

    Flashed messages

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = NameForm()
-	if form.validate_on_submit():
+	if fform.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and Old_name != form.name.data:
+			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
-	return render_template('index.html', form=form, name=session.get('name'))
+		form.name.data = ''
+		return redirect(url_for('index'))
+	return render_template('index.html', form = form, name = session.get('name'))
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True)		
