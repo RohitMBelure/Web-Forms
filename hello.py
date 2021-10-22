a86f737aaf5f57d28c05abf3421addb2061cc544
Rohit <rohitbelure128@gmail.com>
2021-10-22 19:13:47 +0530

    Database configuration

--- hello.py
+++ hello.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy, os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-	form = NameForm()
-	if fform.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and Old_name != form.name.data:
-			flash('Looks like you have changed your name!')
-		session['name'] = form.name.data
-		form.name.data = ''
-		return redirect(url_for('index'))
-	return render_template('index.html', form = form, name = session.get('name'))
+db = SQLAlchemy(app)	
 
-if __name__ == '__main__':
-	app.run(debug=True)		
